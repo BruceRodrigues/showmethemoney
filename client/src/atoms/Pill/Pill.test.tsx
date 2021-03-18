966528adb34e9946b108c7b6d125f92f3807c2f4
Bruce Rodrigues <brucerodrigues6@gmail.com>
2021-03-17 22:53:49 -0300

    Fixing Pill tests + jest config

--- client/src/atoms/Pill/Pill.test.tsx
+++ client/src/atoms/Pill/Pill.test.tsx
@@ -1,23 +1,24 @@
-import React from 'react'
+import '@testing-library/jest-dom/extend-expect'
 import { render } from '@testing-library/react'
+import React from 'react'
 import { Pill } from './Pill'
 
 describe('Pill component', () => {
     it('Should render with positive styles is greater than 0', () => {
-        const { getByRole } = render(<Pill value={9} />)
+        const { getByText } = render(<Pill value={9} />)
 
-        expect(getByRole('span')).toHaveClass('bg-green-500')
+        expect(getByText('9', { selector: 'span' })).toHaveClass('bg-green-500')
     })
 
     it('Should render with negative styles if value is less than 0', () => {
-        const { getByRole } = render(<Pill value={-1} />)
+        const { getByText } = render(<Pill value={-1} />)
 
-        expect(getByRole('span')).toHaveClass('bg-red-500')
+        expect(getByText('-1', { selector: 'span' })).toHaveClass('bg-red-500')
     })
 
     it('Should render with negative styles if value is 0', () => {
-        const { getByRole } = render(<Pill value={0} />)
+        const { getByText } = render(<Pill value={0} />)
 
-        expect(getByRole('span')).toHaveClass('bg-red-500')
+        expect(getByText('0', { selector: 'span' })).toHaveClass('bg-red-500')
     })
 })
