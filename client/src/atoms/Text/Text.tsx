aba429ef69267c998a444ba87b81c4e26a6f14df
Bruce Rodrigues <brucerodrigues6@gmail.com>
2021-03-20 01:40:29 -0300

    Textual components (#15)
    * Heading component
    
    * Text component
    
    * adding size prop

--- client/src/atoms/Text/Text.tsx
+++ client/src/atoms/Text/Text.tsx
@@ -1,13 +1,31 @@
 import React from 'react'
 import { css } from '../../utils'
 
-interface TextProps extends React.HTMLProps<HTMLElement> {
+type FontSize = 'xs' | 'sm' | 'md' | 'lg'
+
+interface TextProps extends Omit<React.HTMLProps<HTMLElement>, 'size'> {
     variant: 'p' | 'span'
+    size: FontSize
 }
 
-const styles = ['text-base', 'font-sans']
+const styles = ['font-sans']
 
-export const Text = ({ variant = 'span', children }: TextProps) => {
+const fontSizeStyles = {
+    xs: 'text-xs',
+    sm: 'text-sm',
+    md: 'text-base',
+    lg: 'text-lg',
+}
+
+export const Text = ({
+    variant = 'span',
+    size = 'md',
+    children,
+}: TextProps) => {
     const Element = variant as 'p' | 'span'
-    return <Element className={css(styles)}>{children}</Element>
+    return (
+        <Element className={css([...styles, fontSizeStyles[size]])}>
+            {children}
+        </Element>
+    )
 }
