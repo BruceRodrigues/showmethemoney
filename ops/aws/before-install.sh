359efc813cd6da2fcbf39e044bc3afe433d16ecf
Bruce Rodrigues <brucerodrigues6@gmail.com>
2021-04-21 18:33:16 -0300

    parameters workaround

--- ops/aws/before-install.sh
+++ ops/aws/before-install.sh
@@ -4,3 +4,17 @@ docker volume prune -f
 docker system prune -f
 docker rmi ops_client
 docker rmi ops_server
+
+touch .env
+
+add_parameter() {
+    PARAM=$1
+    PARAM_VALUE=$(aws --region sa-east-1 ssm get-parameter --name /SMTM/dev/${PARAM} --query Parameter.Value | sed -e 's/^"//' -e 's/"$//')
+    echo "$PARAM=$PARAM_VALUE" >> .env
+}
+
+add_parameter PORT
+add_parameter HOST
+add_parameter NODE_ENV
+add_parameter APP_KEY
+add_parameter DB_CONNECTION
