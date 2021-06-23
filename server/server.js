1375e2b46de105b7a24dc2cfe65703dbcd777558
Bruce Rodrigues <brucerodrigues6@gmail.com>
2021-06-23 00:34:05 -0300

    user post endpoint

--- server/server.js
+++ server/server.js
@@ -1,9 +1,16 @@
 const express = require("express");
+const connectDB = require("./config/db");
 
 const app = express();
 
+connectDB();
+
 const PORT = process.env.PORT || 5000;
+
+app.use(express.json({ extended: false }));
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
 
 app.get("/", (req, res) => res.send("API Running"));
+app.use("/api/users", require("./routes/api/user"));
+app.use("/api/auth", require("./routes/api/auth"));
